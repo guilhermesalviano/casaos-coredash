8ed81907997d96a9e62b274e8083e58b30795f72
Guilherme <guilherme.salviano12@outlook.com>
2026-03-05 18:36:09 -0300

    fix: inactive synchronize option

--- apps/web/lib/db.ts
+++ apps/web/lib/db.ts
@@ -27,7 +27,7 @@ const prodType: DataSourceOptions = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  synchronize: true,
+  synchronize: false,
   logging: true,
 }
 
