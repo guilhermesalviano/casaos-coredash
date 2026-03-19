5abb09a01dc537594d68157d8297cd08ad5b577b
Guilherme <guilherme.salviano12@outlook.com>
2026-03-19 10:39:20 -0300

    feat(todo): reorder priority tasks

--- apps/web/app/api/todo/route.ts
+++ apps/web/app/api/todo/route.ts
@@ -35,6 +35,17 @@ export async function GET(req: NextRequest) {
       return Number(end) >= today.getTime();
     });
 
+    todosFiltered.sort((a, b) => {
+      if (a.priority && !b.priority) return -1;
+      if (!a.priority && b.priority) return 1;
+
+      if (a.priority && b.priority) {
+        return a.title.localeCompare(b.title);
+      }
+
+      return 0;
+    });
+
     const checksResult = await todoCheckRepository.find({
       where: [
         {
