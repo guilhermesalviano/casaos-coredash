3367aed21c2b78cb22e0085d23a71266ed36d739
Guilherme <guilherme.salviano12@outlook.com>
2026-03-23 16:57:05 -0300

    chore: remove settings button

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,6 @@ import SystemsStatus from "@/components/systemsStatus";
 import ThemeToggle from "@/components/themeToggle";
 import ActiveCards from "@/components/activeCards";
 import Logo from "@/components/logo";
-import Settings from "@/components/settings";
 
 export default function Page() {
   return (
@@ -18,7 +17,6 @@ export default function Page() {
           <div className="header-clock"><Clock /></div>
           <div className="header-status gap-4">
             <SystemsStatus />
-            <Settings />
             <ThemeToggle />
           </div>
         </div>
