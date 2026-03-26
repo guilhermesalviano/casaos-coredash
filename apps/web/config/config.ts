09a002e2d7ed62a01c233edc368e2cc8dd838f58
Guilherme <guilherme.salviano12@outlook.com>
2026-03-26 12:21:56 -0300

    feat(mock): brapi return

--- apps/web/config/config.ts
+++ apps/web/config/config.ts
@@ -9,7 +9,7 @@ export const CONFIG = {
   OPEN_METEO_BASE_URL: `${isDev ? MOCK_BASE_URL : "https://api.open-meteo.com"}/v1/forecast`,
   NOMINATIM_BASE_URL: `${isDev ? MOCK_BASE_URL : "https://nominatim.openstreetmap.org"}/reverse`,
   SERPAPI_BASE_URL: `${isDev ? MOCK_BASE_URL : "https://serpapi.com"}/search`,
-  BRAPI_BASE_URL: "https://brapi.dev/api/quote",
+  BRAPI_BASE_URL: `${isDev ? MOCK_BASE_URL : "https://brapi.dev"}/api/quote`,
   MEDIASTACK_BASE_URL: "https://api.mediastack.com/v1/news",
   DEFAULT_TIMEZONE: "America/Sao_Paulo",
   UPDATE_INTERVAL_MS: 1 * 60 * 60, // 1 hour
